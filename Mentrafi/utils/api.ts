f8308ccd83cbed47fce031a63f50857575f21d75
Jaykumar <singhjay65521@gmail.com>
2026-05-16 13:41:54 +0530

    Remove files1

--- Mentrafi/utils/api.ts
+++
@@ -1,7 +0,0 @@
-import { Platform } from "react-native";
-
-const isEmulator = Platform.OS === "android" && !__DEV__;
-
-export const API_URL = isEmulator
-  ? "http://10.0.2.2:3001"
-  : "http://192.168.1.44:3001";
